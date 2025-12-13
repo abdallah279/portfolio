6563c87465901708da4dc0ae49b3bdf188aa461b
abdallah279 <abdallahzeko8@gmail.com>
2025-12-13 22:40:08 +0200

    work on (add more projects)

--- Js/script.js
+++ Js/script.js
@@ -5,15 +5,15 @@ header.classList.toggle('fixed', window.scrollY > 0);
 // Go To Up
 let upBtn = document.querySelector('.up');
 
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function () {
     // Header Fixed
     header.classList.toggle('fixed', window.scrollY > 0);
 
     // Go To UpBtn Show
-    if( window.scrollY > 0){
+    if (window.scrollY > 0) {
         upBtn.style.bottom = `${15}px`;
         upBtn.style.opacity = `${1}`;
-    }else{
+    } else {
         upBtn.style.opacity = 0;
         upBtn.style.bottom = `${-100}px`;
     }
@@ -23,31 +23,48 @@ window.addEventListener('scroll', function(){
 });
 
 
-// set bar
-let barIcone = document.querySelector('.bar');
-barIcone.addEventListener("click", function(){
-    header.classList.toggle('active');
+// Set bar
+const barIcon = document.querySelector('.bar');
+const icon = barIcon.querySelector('i');
+const overlay = document.getElementById('overlay');
+
+barIcon.addEventListener('click', () => {
+    const isActive = header.classList.toggle('active');
+    overlay.classList.toggle('active');
+
+    icon.classList.toggle('fa-times', isActive);
+    icon.classList.toggle('fa-bars', !isActive);
+
+});
+
+// overlay
+overlay.addEventListener('click', () => {
+    header.classList.remove('active');
+    overlay.classList.remove('active');
+    icon.classList.remove('fa-times');
+    icon.classList.add('fa-bars');
 });
 
 // clicked to top
-upBtn.addEventListener('click', function(){
-    window.scrollTo(0,0);
+upBtn.addEventListener('click', function () {
+    window.scrollTo(0, 0);
 });
 
 // section active class
 let sections = document.querySelectorAll("section");
 
-function sectionActive(){
+function sectionActive() {
     let scrollPosition = document.documentElement.scrollTop;
 
-    sections.forEach(section =>{
-        if(scrollPosition >= section.offsetTop - section.offsetHeight * .5 &&
-            scrollPosition < section.offsetTop + section.offsetHeight - section.offsetHeight * .5){
+    sections.forEach(section => {
+        if (scrollPosition >= section.offsetTop - section.offsetHeight * .5 &&
+            scrollPosition < section.offsetTop + section.offsetHeight - section.offsetHeight * .5) {
 
             let sectionId = section.attributes.id.value;
 
             removeActiveClasses();
             addActiveClass(sectionId);
+            overlay.click();
         }
     });
 }
@@ -55,34 +72,34 @@ function sectionActive(){
 let navLinks = document.querySelectorAll("header nav a");
 
 // Remove All Active Classes
-function removeActiveClasses(){
-    navLinks.forEach(li =>{
+function removeActiveClasses() {
+    navLinks.forEach(li => {
         li.classList.remove('active');
     });
 }
 
 // Add Active Class To link Section
-function addActiveClass(id){
+function addActiveClass(id) {
     document.querySelector(`header nav a[href="#${id}"]`).classList.add('active');
 }
 
 navLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    e.preventDefault();
-    let currentId = e.target.attributes.href.value;
-    let section = document.querySelector(currentId);
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        let currentId = e.target.attributes.href.value;
+        let section = document.querySelector(currentId);
 
-    window.scroll({
-      top: section.offsetTop,
-      behavior: "smooth",
+        window.scroll({
+            top: section.offsetTop,
+            behavior: "smooth",
+        });
     });
-  });
 });
 
 // set skills section
 let skills = document.querySelector('.skills');
 
-window.onscroll = function(){
+window.onscroll = function () {
 
     // skills offsetTop
     let skillsOffSetTop = skills.offsetTop;
@@ -97,7 +114,7 @@ window.onscroll = function(){
     let windowScroll = this.pageYOffset;
 
 
-    if(windowScroll > (skillsOffSetTop + skillsOffSetHeight- windowHeight)){
+    if (windowScroll > (skillsOffSetTop + skillsOffSetHeight - windowHeight)) {
 
         let allSpan = document.querySelectorAll('.skills-content .prograss-bar .prog');
 
@@ -111,26 +128,29 @@ window.onscroll = function(){
 // Filter Projects
 
 const items = document.querySelector('#Portfolio .items'),
-Projects = document.querySelectorAll('.portfolio-content .port');
+    Projects = document.querySelectorAll('.portfolio-content .port');
 
-items.onclick =  (item)=>{
+if (items) {
+    items.onclick = (item) => {
 
-    if(item.target.classList.contains('item')){
-        items.querySelector('.active').classList.remove('active');
-        item.target.classList.add('active');
+        if (item.target.classList.contains('item')) {
+            items.querySelector('.active').classList.remove('active');
+            item.target.classList.add('active');
 
-        Projects.forEach(project =>{
-            if(item.target.getAttribute('data-name') == project.getAttribute('data-name') || item.target.getAttribute('data-name') == 'all'){
-                project.classList.remove("show");
-                project.classList.remove('hide');
-                setTimeout(() => {
-                    project.classList.add('show');
-                }, .1);
-            }else{
-                project.classList.add("hide");
-                project.classList.remove("show");
-            }
-    
-        });
+            Projects.forEach(project => {
+                if (item.target.getAttribute('data-name') == project.getAttribute('data-name') || item.target.getAttribute('data-name') == 'all') {
+                    project.classList.remove("show");
+                    project.classList.remove('hide');
+                    setTimeout(() => {
+                        project.classList.add('show');
+                    }, .1);
+                } else {
+                    project.classList.add("hide");
+                    project.classList.remove("show");
+                }
+
+            });
+        }
     }
+
 }
